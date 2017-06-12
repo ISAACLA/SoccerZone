aedac7c90c50c6c06e6784b049705f990d359455
ISAAC LA <isaacla@ISAACs-MacBook-Pro.local>
2017-06-12 16:21:08 -0700

    First Initializing

---
+++ server/config/mongoose.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+let fs = require('fs');
+let path = require('path');
+
+// mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/SoccerZone');
+let models_path = path.join(__dirname, './../model');
+
+fs.readdirSync(models_path).forEach((file)=>{
+  if (file.indexOf('.js')>=0){
+    require(models_path+'/'+file);
+  }
+})
